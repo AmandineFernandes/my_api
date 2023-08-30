84e5f665b680305e7d9eab179f1b2eee9bcd35ae
Amandine Fernandes <fernandesamandine13@gmail.com>
2023-08-30 10:58:20 +0200

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
-5 times do |index|
-    User.create(first_name: "Nom#{index}", )
+# Supprime les données existantes pour éviter les doublons
+Article.delete_all
+
+# Crée de nouveaux articles avec des données générées par Faker
+30.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph(sentence_count: 3)
+  )
 end
 
-Faker::TvShows::TheOffice.quote 
+
